1fba740971bb8107f5ffbb8d77504c78b7d74f6a
Cássio Alves <cassio.alves09@gmail.com>
2018-12-14 13:14:37 -0200

    Projeto inicializado

---
+++ src/test/java/com/kdev/cursomvc/CursomvcApplicationTests.java
@@ -0,0 +1,17 @@
+package com.kdev.cursomvc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CursomvcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
