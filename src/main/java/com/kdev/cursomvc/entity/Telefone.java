cac0bdc8e434a10e0ba9d57f6cf0811ff8eb2fe2
Cássio Alves <cassio.alves09@gmail.com>
2018-12-16 21:53:24 -0200

    Entity

---
+++ src/main/java/com/kdev/cursomvc/entity/Telefone.java
@@ -0,0 +1,14 @@
+package com.kdev.cursomvc.entity;
+
+public class Telefone {
+
+	private String numero;
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+}
