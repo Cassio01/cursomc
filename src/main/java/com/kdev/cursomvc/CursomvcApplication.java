cd3843583a175e810c34f0b2d270b49930e7c7e5
Cássio Alves <cassio.alves09@gmail.com>
2018-12-16 22:46:56 -0200

    Testando a aplicação

--- src/main/java/com/kdev/cursomvc/CursomvcApplication.java
+++ src/main/java/com/kdev/cursomvc/CursomvcApplication.java
@@ -1,14 +1,140 @@
 package com.kdev.cursomvc;
 
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.kdev.cursomvc.entity.Categoria;
+import com.kdev.cursomvc.entity.Cidade;
+import com.kdev.cursomvc.entity.Cliente;
+import com.kdev.cursomvc.entity.Endereco;
+import com.kdev.cursomvc.entity.Estado;
+import com.kdev.cursomvc.entity.ItemPedido;
+import com.kdev.cursomvc.entity.Pagamento;
+import com.kdev.cursomvc.entity.PagamentoComBoleto;
+import com.kdev.cursomvc.entity.PagamentoComCartao;
+import com.kdev.cursomvc.entity.Pedido;
+import com.kdev.cursomvc.entity.Produto;
+import com.kdev.cursomvc.entity.enums.EstadoPagamento;
+import com.kdev.cursomvc.entity.enums.TipoCliente;
+import com.kdev.cursomvc.repository.CategoriaRepository;
+import com.kdev.cursomvc.repository.CidadeRepository;
+import com.kdev.cursomvc.repository.ClienteRepository;
+import com.kdev.cursomvc.repository.EnderecoRepository;
+import com.kdev.cursomvc.repository.EstadoRepository;
+import com.kdev.cursomvc.repository.ItemPedidoRepository;
+import com.kdev.cursomvc.repository.PagamentoRepository;
+import com.kdev.cursomvc.repository.PedidoRepository;
+import com.kdev.cursomvc.repository.ProdutoRepository;
+
 @SpringBootApplication
-public class CursomvcApplication {
+public class CursomvcApplication implements CommandLineRunner {
+	@Autowired
+	private CategoriaRepository repository;
+
+	@Autowired
+	private ProdutoRepository produtoRepository;
+
+	@Autowired
+	private EstadoRepository estadoRepository;
+
+	@Autowired
+	private CidadeRepository cidadeRepository;
+
+	@Autowired
+	private ClienteRepository clienteRepository;
+
+	@Autowired
+	private EnderecoRepository enderecoRepository;
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomvcApplication.class, args);
 	}
 
-}
+	@Override
+	public void run(String... args) throws Exception {
 
+		Categoria cat1 = new Categoria(null, "Informáticqwa");
+		Categoria cat2 = new Categoria(null, "Escritório");
+
+		Produto p1 = new Produto(null, "Computador", 2000.0);
+		Produto p2 = new Produto(null, "Impressora", 800.0);
+		Produto p3 = new Produto(null, "Mouse", 80.0);
+
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+
+		repository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+
+		Estado est1 = new Estado(null, "Bahia");
+		Estado est2 = new Estado(null, "São Paulo");
+
+		Cidade c1 = new Cidade(null, "Barreiras", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+
+		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@email.com", "123-123-132.55", TipoCliente.PESSOA_FISICA);
+		cli1.getTelefones().addAll(Arrays.asList("9999-8888", "3333-9999"));
+
+		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 203", "Jardim", "32220834", cli1, c1);
+		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+
+		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy hh:mm");
+
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:31"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("30/09/2017 11:35"), cli1, e2);
+
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("15/12/2018 00:00"),
+				null);
+		ped2.setPagamento(pagto2);
+
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+
+		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
+
+		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.0, 1, 200.00);
+		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.0, 2, 80.00);
+		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.0, 1, 800.00);
+		
+		ped1.getItens().addAll(Arrays.asList(ip1,ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+		
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip2));
+		
+		itemPedidoRepository.saveAll(Arrays.asList(ip1, ip2, ip3));
+
+	}
+
+}
